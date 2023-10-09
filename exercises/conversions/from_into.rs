e0881487f488c12bf92014457cb91b26f0c12591
Wenyu(Bosco) Huang <huangwenyuu@outlook.com>
2023-10-09 06:14:15 +0000

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        let s = s.split(',').collect::<Vec<&str>>();
+        if s.len() != 2 || s[0].is_empty() {
+            return Person::default();
+        }
+        
+        if let Ok(age) = s[1].to_string().parse::<usize>() {
+            Person {
+                name: String::from(s[0]),
+                age,
+            }
+        } else {
+            return Person::default();
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if scores.get(&team_1_name).is_none() {
+            scores.insert(team_1_name, Team{goals_scored: team_1_score, goals_conceded: team_2_score});
+        } else {
+            let mut team1 = scores.get_mut(&team_1_name).unwrap();
+            team1.goals_scored = team1.goals_scored + team_1_score;
+            team1.goals_conceded = team1.goals_conceded + team_2_score;
+        }
+        if scores.get(&team_2_name).is_none() {
+            scores.insert(team_2_name, Team{goals_scored: team_2_score, goals_conceded: team_1_score});
+        } else {
+            let mut team2 = scores.get_mut(&team_2_name).unwrap();
+            team2.goals_scored = team2.goals_scored + team_2_score;
+            team2.goals_conceded = team2.goals_conceded + team_1_score;
+        }
+        
     }
     scores
 }
